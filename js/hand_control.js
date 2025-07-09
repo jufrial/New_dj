11d4109daaf136e01783d5936f4ac1d491405add
jufrial <camcerintrovrt@gmail.com>
2025-07-10 01:54:50 +0800

    Update hand_control.js

--- js/hand_control.js
+++ js/hand_control.js
@@ -1,1 +1,26 @@
-// Hand control script for movement/rotation of hand and arm
+export function setupAnalogControl(handGroup) {
+  const joystick = document.getElementById('joystick');
+
+  let dragging = false;
+  let startX, startY;
+
+  joystick.addEventListener('touchstart', (e) => {
+    dragging = true;
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+  });
+
+  joystick.addEventListener('touchmove', (e) => {
+    if (!dragging) return;
+
+    const dx = e.touches[0].clientX - startX;
+    const dy = e.touches[0].clientY - startY;
+
+    handGroup.rotation.y = dx * 0.01;
+    handGroup.rotation.x = dy * 0.01;
+  });
+
+  joystick.addEventListener('touchend', () => {
+    dragging = false;
+  });
+}
