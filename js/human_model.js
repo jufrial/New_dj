c99d66ff0f3eea88e8282bd2a221709565a216be
jufrial <camcerintrovrt@gmail.com>
2025-07-10 04:09:05 +0800

    Update human_model.js

--- js/human_model.js
+++ js/human_model.js
@@ -1,1 +1,17 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
+import { createHead } from './head.js';
+import { createTorso } from './torso.js';
+import { createArms } from './arm.js';
+import { createLegs } from './leg.js';
+
+export function createHumanModel() {
+  const human = new THREE.Group();
+
+  human.add(createHead());
+  human.add(createTorso());
+  human.add(createArms());
+  human.add(createLegs());
+
+  return human;
+}
 
