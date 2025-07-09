434aa78b1f55a941ad4bded4d69ccf406c325dc0
jufrial <camcerintrovrt@gmail.com>
2025-07-10 06:00:11 +0800

    Update hand_control.js

--- js/hand_control.js
+++ js/hand_control.js
@@ -1,1 +1,9 @@
-// hand_control.js - code akan diisi nanti
+// Sistem kontrol tangan masih dummy, nanti bisa dikembangkan gesture
+export const handState = {
+  isGrabbing: false,
+};
+
+export function toggleGrab() {
+  handState.isGrabbing = !handState.isGrabbing;
+  console.log("Tangan:", handState.isGrabbing ? "Genggam" : "Lepas");
+}
