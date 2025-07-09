31550f16d8bd32478d80903c92bb795c467a2dcf
jufrial <camcerintrovrt@gmail.com>
2025-07-10 04:44:44 +0800

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,22 +1,5 @@
-// js/joystick.js
-import { movement } from './movement.js';
-
-const joystick = nipplejs.create({
-  zone: document.body,
-  mode: 'static',
-  position: { left: '60px', bottom: '60px' },
-  color: 'blue',
-  size: 100
-});
-
-joystick.on('move', (evt, data) => {
-  if (data && data.vector) {
-    movement.x = data.vector.x;
-    movement.y = data.vector.y;
-  }
-});
-
-joystick.on('end', () => {
-  movement.x = 0;
-  movement.y = 0;
-});
+// js/movement.js
+export const movement = {
+  x: 0,
+  y: 0
+};
