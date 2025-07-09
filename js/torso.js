29c3a8591b5d0e7b4ed309288f82defb2c810a9a
jufrial <camcerintrovrt@gmail.com>
2025-07-10 06:04:04 +0800

    Update torso.js

--- js/torso.js
+++ js/torso.js
@@ -1,1 +1,9 @@
-// torso.js - code akan diisi nanti
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
+
+export function createTorso() {
+  const torsoGeometry = new THREE.CylinderGeometry(0.3, 0.4, 1.2, 16);
+  const torsoMaterial = new THREE.MeshStandardMaterial({ color: 0xffaaaa });
+  const torso = new THREE.Mesh(torsoGeometry, torsoMaterial);
+  torso.position.y = 0.9;
+  return torso;
+}
