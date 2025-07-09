efb1f8b836ffe7947a4518ac1c8d7a0049e2b819
jufrial <camcerintrovrt@gmail.com>
2025-07-10 05:59:28 +0800

    Update joystick.js

--- js/joystick.js
+++ js/joystick.js
@@ -1,1 +1,25 @@
-// joystick.js - code akan diisi nanti
+export const movement = { x: 0, y: 0 };
+
+const joystick = document.getElementById("joystick");
+
+let startX, startY;
+
+joystick.addEventListener("touchstart", (e) => {
+  const touch = e.touches[0];
+  startX = touch.clientX;
+  startY = touch.clientY;
+});
+
+joystick.addEventListener("touchmove", (e) => {
+  const touch = e.touches[0];
+  const deltaX = touch.clientX - startX;
+  const deltaY = touch.clientY - startY;
+
+  movement.x = deltaX / 50;
+  movement.y = deltaY / 50;
+});
+
+joystick.addEventListener("touchend", () => {
+  movement.x = 0;
+  movement.y = 0;
+});
