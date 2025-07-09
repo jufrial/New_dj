d229cf7418b2a65e7f8475128cb4aa99b69a5fbb
jufrial <camcerintrovrt@gmail.com>
2025-07-10 04:37:17 +0800

    Update human_model.js

--- js/human_model.js
+++ js/human_model.js
@@ -7,29 +7,10 @@ import { createLegs } from './leg.js';
 export function createHumanModel() {
   const human = new THREE.Group();
 
-  // Tambahkan semua bagian tubuh
-  const head = createHead();
-  const torso = createTorso();
-  const arms = createArms();
-  const legs = createLegs();
-
-  // Gabungkan posisi agar menyatu secara vertikal
-  head.position.y = 0;
-  torso.position.y = 0;
-  arms.position.y = 0;
-  legs.position.y = 0;
-
-  // Tambah ke tubuh utama
-  human.add(head);
-  human.add(torso);
-  human.add(arms);
-  human.add(legs);
-
-  // Optional: Skala jika tubuh terlalu besar/kecil
-  human.scale.set(1, 1, 1);
-
-  // Optional: Posisikan karakter di tengah dunia
-  human.position.set(0, 0, 0);
+  human.add(createHead());
+  human.add(createTorso());
+  human.add(createArms());
+  human.add(createLegs());
 
   return human;
 }
