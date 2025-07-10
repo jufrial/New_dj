3c06f8e128b0be7a2f3ea29d5754eec60cdbd747
jufrial <camcerintrovrt@gmail.com>
2025-07-11 00:43:24 +0800

    Update arm.js

--- js/human_model/arm.js
+++ js/human_model/arm.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
+import { createHand } from './hand.js';
 
 export function createArm(side = "left") {
   const arm = new THREE.Group();
@@ -14,6 +15,14 @@ export function createArm(side = "left") {
     new THREE.MeshStandardMaterial({ color: 0xffbbbb })
   );
   lowerArm.position.y = -0.45;
+
+  // Tambahkan tangan di ujung lowerArm
+  const hand = createHand();
+  hand.position.y = -0.25;
+  lowerArm.add(hand);
+
+  // tandai untuk kontrol
+  hand.userData.isHand = true;
 
   const elbow = new THREE.Object3D();
   elbow.position.y = -0.5;
