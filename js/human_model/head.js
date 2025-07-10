7acdc6d6da8f13b2600fa12cfa6e0f3763c8518a
jufrial <camcerintrovrt@gmail.com>
2025-07-10 10:16:29 +0800

    Update head.js

--- js/human_model/head.js
+++ js/human_model/head.js
@@ -1,1 +1,7 @@
-// head.js - code akan diisi nanti
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
+
+export function createHead() {
+  const geometry = new THREE.SphereGeometry(0.25, 16, 16);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffdddd });
+  return new THREE.Mesh(geometry, material);
+}
